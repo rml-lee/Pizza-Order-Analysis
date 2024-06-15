3cf80bbafce173c4bb9c1b40dfa2d8f9975f5e26
raheem-gsu <leeraheem@icloud.com>
2024-06-15 14:05:38 -0400

    update

--- Pizza Project.sql
+++ Pizza Project.sql
@@ -28,7 +28,7 @@ FROM
 
 # Data Analysis
 
--- 1. What's the amount of orders received daily from pizzas that contained mushrooms and tomatoes as part of their toppings in January?
+-- 1. What are the volume of orders received daily from pizzas that contained mushrooms and tomatoes as part of their toppings in January?
 SELECT
     DAY(timestamp) AS day,
     COUNT(DISTINCT o.order_id) AS total_orders
@@ -66,45 +66,22 @@ ORDER BY 1 ASC;
 
 
 
--- 3. Return the pizza_id's of orders that contain 3 different pizza sizes.
+-- 3. What is the percentage of pizzas ordered of each size ('S', 'M', 'L', 'XL') across the entire year?
 SELECT
-    pizza_id
-FROM
-    order_details
-WHERE
-    order_id IN
-    (SELECT
-         o.order_id
-     FROM
-         orders o
-         JOIN order_details od
-         ON o.order_id = od.order_id
-         JOIN pizzas p
-         ON p.pizza_id = od.pizza_id
-     GROUP BY 1
-     HAVING
-         COUNT(DISTINCT size) = 3);
-
-
-
--- 4. What is the percentage of large pizzas ordered during December 13th?
-SELECT
-    (SUM(CASE
-            WHEN p.size = 'L' THEN 1
-            ELSE 0
-        END) / COUNT(*)) * 100 AS percentage
+    ROUND((SUM(IF(p.size = 'S', 1, 0)) / COUNT(*)) * 100, 0) AS small_percentage,
+    ROUND((SUM(IF(p.size = 'M', 1, 0)) / COUNT(*)) * 100, 0) AS medium_percentage,
+    ROUND((SUM(IF(p.size = 'L', 1, 0)) / COUNT(*)) * 100, 0) AS large_percentage,
+    ROUND((SUM(IF(p.size = 'XL', 1, 0)) / COUNT(*)) * 100, 0) AS extra_large_percentage
 FROM
     orders o
     JOIN order_details od
     ON o.order_id = od.order_id
     JOIN pizzas p
-    ON p.pizza_id = od.pizza_id
-WHERE
-    DATE(timestamp) = '2015-12-13';
+    ON p.pizza_id = od.pizza_id;
 
 
 
--- 5. What is the quarterly revenue generated from each size of "The Greek Pizza"?
+-- 4. What is the quarterly revenue generated from each size of "The Greek Pizza"?
 SELECT
     QUARTER(timestamp) AS quarter,
     size,
@@ -122,7 +99,7 @@ ORDER BY 1 ASC, 2 ASC;
 
 
 
--- 6. Return a summary of revenue generated for every pizza type sold on July 6th.
+-- 5. Return a summary of revenue generated for every pizza type sold on July 6th.
 -- In case a certain pizza didn't sell, include it in the results as 0.
 SELECT
     pt.name,
@@ -147,91 +124,9 @@ FROM
 
 
 
--- 7. List all order id's that occurred after 12pm on November 3rd using a CTE.
-WITH morn_orders AS
-         (SELECT
-              order_id,
-              timestamp
-          FROM
-              orders
-          WHERE
-                DATE(timestamp) = '2015-11-03'
-            AND EXTRACT(HOUR FROM timestamp) < 12)
+-- 6. Return the revenue and running total of revenue generated monthly from Chicken Alfredo Pizzas.
 SELECT
-    o.order_id,
-    o.timestamp AS post_morning_timestamp
-FROM
-    orders o
-    LEFT JOIN morn_orders mo
-    ON mo.timestamp = o.timestamp
-WHERE
-    DATE(o.timestamp) = '2015-11-03'
-    AND mo.timestamp IS NULL;
-
-
-
--- 8. On November 3rd, count the amount of orders that occurred in the morning and the amount during the Afternoon/Evening using a CTE.
-WITH morn_orders AS
-         (SELECT
-              order_id,
-              timestamp
-          FROM
-              orders
-          WHERE
-                DATE(timestamp) = '2015-11-03'
-            AND EXTRACT(HOUR FROM timestamp) < 12)
-SELECT
-    CASE
-        WHEN mo.timestamp IS NOT NULL THEN 'Morning'
-        ELSE 'Afternoon/Evening'
-    END AS time_of_day,
-    COUNT(*) AS total_orders
-FROM
-    orders o
-    LEFT JOIN morn_orders mo
-    ON mo.timestamp = o.timestamp
-WHERE
-    DATE(o.timestamp) = '2015-11-03'
-GROUP BY 1;
-
-
-
--- 9. Which day has the highest average order volume per hour during the month of August?
-
-WITH cte AS -- Returns the number of orders received per hour daily in Aug.
-         (SELECT
-              DATE(timestamp) AS day,
-              HOUR(timestamp) AS hour,
-              COUNT(DISTINCT o.order_id) AS total_orders
-          FROM
-              orders o
-              JOIN order_details od
-              ON o.order_id = od.order_id
-          WHERE
-              MONTH(timestamp) = 08
-          GROUP BY 1, 2
-          ORDER BY 1 ASC, 2 ASC),
-
-     cte2 AS -- Returns the average amount of orders received daily
-         (SELECT
-              day,
-              AVG(total_orders) AS avg_orders
-          FROM
-              cte
-          GROUP BY 1)
-SELECT -- Returns the the day we received the highest avg amount of pizza orders
-    day,
-    avg_orders
-FROM
-    cte2
-WHERE
-    avg_orders = (SELECT MAX(avg_orders) FROM cte2);
-
-
-
--- 10. Return the revenue and running total of revenue generated daily from Chicken Alfredo Pizzas during the month of February.
-SELECT
-    DATE(o.timestamp) AS day,
+    MONTH(o.timestamp) AS month,
     SUM(d.quantity * p.price) AS revenue,
     SUM(SUM(d.quantity * p.price)) OVER (ORDER BY DATE(o.timestamp) ASC) AS running_total
 FROM
@@ -242,40 +137,42 @@ FROM
     ON d.order_id = o.order_id
 WHERE
       d.pizza_id LIKE '%alfredo%'
-  AND MONTH(o.timestamp) = 02
 GROUP BY 1;
 
 
 
--- 11. What are the top 2 busiest times of the week? (3rd week of Apr.)
+-- 7. What are the top two busiest times of the day on average per week?
 SELECT
     day_of_week,
     time_of_day,
-    total_orders
+    avg_total_orders
 FROM
     (SELECT
-         DAYNAME(timestamp) AS day_of_week,
-         CASE
-             WHEN EXTRACT(HOUR FROM timestamp) < 12 THEN 'Morning'
-             WHEN EXTRACT(HOUR FROM timestamp) BETWEEN 12 AND 15 THEN 'Early Afternoon'
-             WHEN EXTRACT(HOUR FROM timestamp) > 15 THEN 'Late Afternoon'
-         END AS time_of_day,
-         COUNT(DISTINCT o.order_id) AS total_orders,
-         DENSE_RANK() OVER (ORDER BY COUNT(DISTINCT o.order_id) DESC) AS rnk
+         day_of_week,
+         time_of_day,
+         ROUND(AVG(total_orders), 0) AS avg_total_orders,
+         DENSE_RANK() OVER (ORDER BY ROUND(AVG(total_orders), 0) DESC) AS rnk
      FROM
-         orders o
-         JOIN order_details od
-         ON o.order_id = od.order_id
-     WHERE
-           MONTH(timestamp) = 04
-       AND DAY(timestamp) BETWEEN 19 AND 25
-     GROUP BY 1, 2) t
+         (SELECT
+              WEEK(timestamp) AS week,
+              DAYNAME(timestamp) AS day_of_week,
+              CASE
+                  WHEN EXTRACT(HOUR FROM timestamp) < 12 THEN 'Morning'
+                  WHEN EXTRACT(HOUR FROM timestamp) BETWEEN 12 AND 15 THEN 'Early Afternoon'
+                  WHEN EXTRACT(HOUR FROM timestamp) > 15 THEN 'Late Afternoon'
+              END AS time_of_day,
+              COUNT(*) AS total_orders
+          FROM
+              orders
+          GROUP BY 1, 2, 3) t
+     GROUP BY 1, 2) t2
 WHERE
     rnk <= 2;
 
 
 
--- 12. What is the revenue share percentage of each pizza category?
+
+-- 8. What is the revenue share percentage of each pizza category?
 SELECT
     category,
     ROUND((SUM(d.quantity * p.price) / r.total_revenue) * 100, 0) AS rev_percentage
@@ -298,8 +195,7 @@ GROUP BY 1;
 
 
 
-
--- 13. Compare the sales (in percentage) of pizzas on May 22 and June 22. Which pizzas had an increase in sales from May to June?
+-- 9. Compare the sales (in percentage) of pizzas on May 22 and June 22. Which pizzas had an increase in sales from May to June?
 WITH main AS
          (SELECT
               timestamp,
@@ -352,9 +248,57 @@ FROM
     JOIN pizza_types pt
     ON pt.pizza_type_id = s.pizza_type_id
 WHERE
-    sales_variance > 0;
+    sales_variance > 0
+ORDER BY 2 DESC;
 
 
+
+-- 10. What are the order id's of orders that were made after 12pm on November 3rd?
+WITH morn_orders AS
+         (SELECT
+              order_id,
+              timestamp
+          FROM
+              orders
+          WHERE
+                DATE(timestamp) = '2015-11-03'
+            AND EXTRACT(HOUR FROM timestamp) < 12)
+SELECT
+    o.order_id,
+    o.timestamp AS post_morning_timestamp
+FROM
+    orders o
+    LEFT JOIN morn_orders mo
+    ON mo.timestamp = o.timestamp
+WHERE
+    DATE(o.timestamp) = '2015-11-03'
+    AND mo.timestamp IS NULL;
+
+
+
+-- 11. How many orders did we receive in the morning? How many orders did we receive in the Afternoon & Evening?
+WITH morn_orders AS
+         (SELECT
+              order_id,
+              timestamp
+          FROM
+              orders
+          WHERE
+                DATE(timestamp) = '2015-11-03'
+            AND EXTRACT(HOUR FROM timestamp) < 12)
+SELECT
+    CASE
+        WHEN mo.timestamp IS NOT NULL THEN 'Morning'
+        ELSE 'Afternoon/Evening'
+    END AS time_of_day,
+    COUNT(*) AS total_orders
+FROM
+    orders o
+    LEFT JOIN morn_orders mo
+    ON mo.timestamp = o.timestamp
+WHERE
+    DATE(o.timestamp) = '2015-11-03'
+GROUP BY 1;
 
 
 
